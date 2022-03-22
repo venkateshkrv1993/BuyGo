aec049c5e0458209185c34a560e4deebc6a2e691
apple <venkateshwaran@estrrado.com>
2022-03-22 21:05:10 +0530

    Initial commit

--- app/src/main/java/com/vajro/buygo/adapters/ProductsAdapter.kt
+++ app/src/main/java/com/vajro/buygo/adapters/ProductsAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.vajro.buygo.R
 import com.vajro.buygo.listeners.OnUpdateListener
 import com.vajro.buygo.databinding.AdapterItemProductBinding
 import com.vajro.buygo.models.ProductData
@@ -35,6 +37,12 @@ class ProductsAdapter(
         return ViewHolder(binding)
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        if (payloads.size > 0) {
+            updateQty(holder, position)
+        } else onBindViewHolder(holder, position)
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val model = productsFiltered[position]
@@ -43,6 +51,8 @@ class ProductsAdapter(
         holder.binding.tvPrice.text = model.special
         holder.binding.tvOldPrice.text = model.price
         holder.binding.tvQty.text = model.qty.toString()
+
+        updateQty(holder, position)
 
         Glide.with(context)
             .load(model.thumb)
@@ -53,12 +63,12 @@ class ProductsAdapter(
                 this.qty = this.qty + 1
             }
             val item = productsFiltered[holder.adapterPosition]
-            holder.binding.tvQty.text = item.qty.toString()
             if (item.qty == 1) {
                 onUpdateListener?.onInsert(item)
             } else {
                 onUpdateListener?.onUpdate(item.product_id, item.qty)
             }
+            notifyItemChanged(holder.adapterPosition, "UPDATE")
         }
 
         holder.binding.btnMinus.setOnClickListener {
@@ -67,11 +77,23 @@ class ProductsAdapter(
                     this.qty = this.qty - 1
                 }
                 val item = productsFiltered[holder.adapterPosition]
-                holder.binding.tvQty.text = item.qty.toString()
                 onUpdateListener?.onUpdate(item.product_id, item.qty)
+                notifyItemChanged(holder.adapterPosition, "UPDATE")
             }
         }
 
+    }
+
+    fun updateQty(holder: ViewHolder, position: Int) {
+        val model = productsFiltered[position]
+        holder.binding.tvQty.text = model.qty.toString()
+        holder.binding.cardView.setCardBackgroundColor(
+            ContextCompat.getColor(
+                context,
+                if (model.qty > 0) R.color.pink_50 else R.color.white
+            )
+        )
+        holder.binding.cardView.elevation = if (model.qty > 0) 10f else 2f
     }
 
     override fun getItemCount(): Int {
